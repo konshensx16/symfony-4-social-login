72f6dea3a586e553426d01a3e29fe72c5f61a193
Overseas Media <konshensx16@gmail.com>
2019-09-11 19:43:23 +0100

    initial commit

---
+++ src/Entity/User.php
@@ -0,0 +1,197 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="json")
+     */
+    private $roles = [];
+
+    /**
+     * @var string The hashed password
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $facebookID;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $facebookAccessToken;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $githubID;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $githubAccessToken;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getPassword(): string
+    {
+        return (string) $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getSalt()
+    {
+        // not needed when using the "bcrypt" algorithm in security.yaml
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFacebookID()
+    {
+        return $this->facebookID;
+    }
+
+    /**
+     * @param mixed $facebookID
+     */
+    public function setFacebookID($facebookID): void
+    {
+        $this->facebookID = $facebookID;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFacebookAccessToken()
+    {
+        return $this->facebookAccessToken;
+    }
+
+    /**
+     * @param mixed $facebookAccessToken
+     */
+    public function setFacebookAccessToken($facebookAccessToken): void
+    {
+        $this->facebookAccessToken = $facebookAccessToken;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGithubID()
+    {
+        return $this->githubID;
+    }
+
+    /**
+     * @param mixed $githubID
+     */
+    public function setGithubID($githubID): void
+    {
+        $this->githubID = $githubID;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGithubAccessToken()
+    {
+        return $this->githubAccessToken;
+    }
+
+    /**
+     * @param mixed $githubAccessToken
+     */
+    public function setGithubAccessToken($githubAccessToken): void
+    {
+        $this->githubAccessToken = $githubAccessToken;
+    }
+}
---
+++ src/Security/MyEntityUserProvider.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\User;
+
+use HWI\Bundle\OAuthBundle\Connect\AccountConnectorInterface;
+use HWI\Bundle\OAuthBundle\OAuth\Response\UserResponseInterface;
+use HWI\Bundle\OAuthBundle\Security\Core\User\EntityUserProvider;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class MyEntityUserProvider extends EntityUserProvider implements AccountConnectorInterface {
+
+    public function loadUserByOAuthUserResponse(UserResponseInterface $response)
+    {
+        $resourceOwnerName = $response->getResourceOwner()->getName();
+
+        if (!isset($this->properties[$resourceOwnerName])) {
+            throw new \RuntimeException(sprintf("No property defined for entity for resource owner '%s'.", $resourceOwnerName));
+        }
+
+        $serviceName = $response->getResourceOwner()->getName();
+        $setterId = 'set'. ucfirst($serviceName) . 'ID';
+        $setterAccessToken = 'set'. ucfirst($serviceName) . 'AccessToken';
+
+        // unique integer
+        $username = $response->getUsername();
+        if (null === $user = $this->findUser(array($this->properties[$resourceOwnerName] => $username))) {
+            // TODO: Create the user
+            $user = new User();
+
+            $user->setEmail($response->getEmail());
+
+            $user->$setterId($username);
+            $user->$setterAccessToken($response->getAccessToken());
+
+            $this->em->persist($user);
+            $this->em->flush();
+
+            return $user;
+        }
+        // JUST FOR FACEBOOK
+        $user->setFacebookAccessToken($response->getAccessToken());
+
+        return $user;
+    }
+
+    /**
+     * Connects the response to the user object.
+     *
+     * @param UserInterface $user The user object
+     * @param UserResponseInterface $response The oauth response
+     */
+    public function connect(UserInterface $user, UserResponseInterface $response)
+    {
+        if (!$user instanceof User) {
+            throw new UnsupportedUserException(sprintf('Expected an instance of App\Model\User, but got "%s".', get_class($user)));
+        }
+
+        $property = $this->getProperty($response);
+        $username = $response->getUsername();
+
+        if (null !== $previousUser = $this->registry->getRepository(User::class)->findOneBy(array($property => $username))) {
+            // 'disconnect' previously connected users
+            $this->disconnect($previousUser, $response);
+        }
+
+
+        $serviceName = $response->getResourceOwner()->getName();
+        $setter = 'set'. ucfirst($serviceName) . 'AccessToken';
+
+        $user->$setter($response->getAccessToken());
+
+        $this->updateUser($user, $response);
+    }
+
+    /**
+     * ##STOLEN#
+     * Gets the property for the response.
+     *
+     * @param UserResponseInterface $response
+     *
+     * @return string
+     *
+     * @throws \RuntimeException
+     */
+    protected function getProperty(UserResponseInterface $response)
+    {
+        $resourceOwnerName = $response->getResourceOwner()->getName();
+
+        if (!isset($this->properties[$resourceOwnerName])) {
+            throw new \RuntimeException(sprintf("No property defined for entity for resource owner '%s'.", $resourceOwnerName));
+        }
+
+        return $this->properties[$resourceOwnerName];
+    }
+
+    /**
+     * Disconnects a user.
+     *
+     * @param UserInterface $user
+     * @param UserResponseInterface $response
+     * @throws \TypeError
+     */
+    public function disconnect(UserInterface $user, UserResponseInterface $response)
+    {
+        $property = $this->getProperty($response);
+        $accessor = PropertyAccess::createPropertyAccessor();
+
+        $accessor->setValue($user, $property, null);
+
+        $this->updateUser($user, $response);
+    }
+
+    /**
+     * Update the user and persist the changes to the database.
+     * @param UserInterface $user
+     * @param UserResponseInterface $response
+     */
+    private function updateUser(UserInterface $user, UserResponseInterface $response)
+    {
+        $user->setEmail($response->getEmail());
+        // TODO: Add more fields?!
+
+        $this->em->persist($user);
+        $this->em->flush();
+    }
+}
